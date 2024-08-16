61564306e0ff20a16c0bc50a7f7a5fd7095e72cb
aldemaas <aldemirc22@gmail.com>
2024-08-16 09:19:54 -0300

    add questao4 (teste)

---
+++ src/conjunto_1_sequenciais_entrada_calculo_e_saida/Questao4.java
@@ -0,0 +1,26 @@
+package conjunto_1_sequenciais_entrada_calculo_e_saida;
+
+import java.util.Scanner;
+
+public class Questao4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota do aluno");
+        double nota1 = sc.nextDouble();
+        System.out.println("Digite a segunda nota do aluno");
+        double nota2 = sc.nextDouble();
+        System.out.println("Digite a terceira nota do aluno");
+        double nota3 = sc.nextDouble();
+        System.out.println("Digite a quarta nota do aluno");
+        double nota4 = sc.nextDouble();
+
+        double mediaAluno = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média final do aluno é: " + mediaAluno);
+
+    }
+
+}
