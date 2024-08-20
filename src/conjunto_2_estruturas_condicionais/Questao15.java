c27584e3d44b2fe04b5e12efd2387eaf4c936fe0
PedroMessiasxD <pemlucena@hotmail.com>
2024-08-19 22:06:39 -0300

    Questões de Pedro

--- src/conjunto_2_estruturas_condicionais/Questao15.java
+++ src/conjunto_2_estruturas_condicionais/Questao15.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Questao15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Informe o dia do seu aniversário.");
+        System.out.println("Informe o dia do seu aniversário:");
 
         int day = sc.nextInt();
 
